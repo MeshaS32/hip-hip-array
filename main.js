7ef5f6bcce1cefd60c25738061c5c7cf9a22a493
MeshaS32 <kamesha.hamilton@codeimmersives.com>
2022-09-27 21:14:53 -0400

    main.js

--- main.js
+++ main.js
@@ -26,7 +26,13 @@ const students = [
   ];
   // Challenge 1 Code
 
+let i = 0;
 
+
+while(i < students.length){
+  console.log(students[i])
+  i++
+}
 
   console.log("\n");
   console.log("Challenge 2");
@@ -35,8 +41,14 @@ const students = [
   const grades = [100, 80, 110, 75, 83, 64];
   //Challenge 2 Code
 
-  
+console.log(grades.reverse())
 
+index = 0
+
+while(index < grades.length){
+  console.log(grades[index])
+  index++
+}
 
   console.log("\n");
   console.log("Challenge 3");
@@ -45,6 +57,13 @@ const students = [
   const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
   // Challenge 3 Code
 
+let i1 = 0
+
+  while(i1 < positiveNumbers.length){
+if(positiveNumbers[i1] % 2 === 0){
+  console.log(positiveNumbers[i1])
+} i1++
+  }
 
 
   console.log("\n");
@@ -54,7 +73,14 @@ const students = [
   const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
 
   // Challenge 4 Code
+  
+  let i2 = -10
 
+  while(i2 < mixedSignNumbers.length){
+if(mixedSignNumbers[i2] % 2 === 0){
+  console.log(mixedSignNumbers[i2])
+} i2++
+  }
 
 
   
@@ -65,7 +91,20 @@ const students = [
   const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
   // Challenge 5 Code
     
+  
+  symmetricalCapitals.shift()
+  symmetricalCapitals.shift()
+  symmetricalCapitals.pop()
+  
+  let i3 = 0;
 
+
+  while(i3 < symmetricalCapitals.length){
+    console.log(symmetricalCapitals[i3])
+    i3++
+  }
+  
+  
 
   console.log("\n");
   console.log("Challenge 6");
@@ -74,7 +113,18 @@ const students = [
   const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
   // Challenge 6 Code
   
-  
+  (fibonacciNumbers.unshift("0"))
+  console.log(fibonacciNumbers)
+  fibonacciNumbers.push(["21","34"])
+  console.log(fibonacciNumbers)
+
+  let i4 = 0;
+
+
+while(i4 < fibonacciNumbers.length){
+  console.log(fibonacciNumbers[i4])
+  i4++
+}
 
 
   console.log("\n");
@@ -83,8 +133,14 @@ const students = [
   // Make a NEW array with 5 values of your choice, using the same syntax as the array creation from the previous 6 challenges; that is, every value should be placed within the square brackets.  Now console.log out each value individually.
   // Challenge 7 Code
 
+  let myArray2 = ["I", "will", "be", "a", "millionare"]
+  
+  let i5 = 0
 
-
+  while(i5 < myArray2.length){
+    console.log(myArray2[i5])
+    i5++
+  }
 
 
   console.log("\n");
@@ -94,7 +150,18 @@ const students = [
   const emptyArray = [];
   // Challenge 8 Code
 
+  emptyArray.unshift("a")
+  emptyArray.unshift("live")
+  emptyArray.unshift("I")
+  emptyArray.push("happy", "life!")
+  console.log(emptyArray)
 
+  let i6 = 0
+
+  while(i6 < emptyArray.length){
+    console.log(emptyArray[i6])
+    i6++
+  }
 
 
   console.log("\n");
@@ -102,7 +169,13 @@ const students = [
   // Challenge 9
   // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
   // Challenge 9 Code
-  
+
+
+let i7 = 3
+while(i7 < 11){
+  console.log(students[i7])
+  i7++
+}
   
   console.log("\n");
   console.log("Challenge 10");
@@ -111,7 +184,25 @@ const students = [
   // Do NOT use slice, you will be using that for the next challenge!
   // Challenge 10 Code
 
+let aCopy = []
 
+index3 = 3;
+
+while(index3 < 11){
+ aCopy.push(students[index3])
+index3++
+console.log(aCopy)
+}
+console.log(aCopy)
+
+
+let index4 = 0;
+
+
+while(index4 < aCopy.length){
+  console.log(aCopy[index4])
+  index4++
+}
 
 
   console.log("\n");
@@ -120,6 +211,19 @@ const students = [
   // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
   // Challenge 11 Code
   
+
+let aCopy1 = students.slice(3,11)
+  console.log(aCopy1)
+
+  let i9 = 0;
+
+
+while(i9 < aCopy1.length){
+  console.log(aCopy1[i9])
+  i9++
+}
+console.log("Original students: ")
+console.log(students)
 
 
 
@@ -131,15 +235,26 @@ const students = [
   // Challenge 12 Code
 
   
-  
-  
+  let splice = dinosaurs.splice(4,3)
+  console.log(splice)
+  console.log(dinosaurs)
+
+  let index5 = 0;
+
+
+while(index5 < dinosaurs.length){
+  console.log(dinosaurs[index5])
+  index5++
+}
+
   console.log("\n");
   console.log("Challenge 13");
   // Challenge 13
   // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the "separator". Then console.log out that string.
   // Challenge 13 Code
 
-
+let dinosaurs2 = dinosaurs.join("*")
+console.log(dinosaurs2)
 
   
   console.log("\n");
@@ -149,7 +264,9 @@ const students = [
   // Use .reverse to reverse the dinosaur array and then log out the original array. .reverse DOES change the original array.
   // Challenge 14 Code
 
-
+console.log(dinosaurs.reverse())
+console.log("Original dinosaurs")
+console.log(dinosaurs.reverse())
 
   
   console.log("\n");
@@ -161,3 +278,18 @@ const students = [
 
 
   
+let states =["New York", "Maryland"]
+let states2 = ["Washington", "Florida"]
+
+let states3 = (states.concat(states2))
+console.log(states.concat(states2))
+
+let i10 = 0;
+
+
+while(i10 < states3.length){
+  console.log(states3[i10])
+  i10++
+}
+console.log(states)
+console.log(states2)
